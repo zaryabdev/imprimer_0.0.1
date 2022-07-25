0829965992afd76ac1860e90522b599bd6c0458d
zaryabdev <zaryabdev404@gmail.com>
2022-07-25 19:50:35 +0500

    edit

---
+++ src/ReactComponent.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function ReactComponent() {
+    const [profileInfo, setProfileInfo] = useState(null);
+    const formHandler = async (e) => {
+        e.preventDefault();
+
+    };
+}
